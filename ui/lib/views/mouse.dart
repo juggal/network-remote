20a4ded23cd51f98823d96f798adfb9f1098571a
nihar <dasnihar48@gmail.com>
2021-05-11 02:17:11 +0530

    added custom delta calculations

--- ui/lib/views/mouse.dart
+++ ui/lib/views/mouse.dart
@@ -4,12 +4,32 @@ import 'package:flutter/material.dart';
 import 'package:network_remote/widgets/mouse_button.dart';
 import 'package:web_socket_channel/io.dart';
 
-class Mouse extends StatelessWidget {
+class Mouse extends StatefulWidget {
   Mouse({
     @required this.channel,
   });
 
   final IOWebSocketChannel channel;
+
+  @override
+  _MouseState createState() => _MouseState();
+}
+
+class _MouseState extends State<Mouse> {
+  double _prevX;
+
+  double _prevY;
+
+  void hanldePointerMove(X, Y) {
+    double deltaX = X - this._prevX;
+    double deltaY = Y - this._prevY;
+
+    this._prevX = X;
+    this._prevY = Y;
+
+    final data = {"type": "moveMouse", "dx": deltaX, "dy": deltaY};
+    this.widget.channel.sink.add(json.encode(data));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,17 +42,13 @@ class Mouse extends StatelessWidget {
             child: ConstrainedBox(
               constraints: BoxConstraints.tight(const Size.fromWidth(450)),
               child: Listener(
-                onPointerMove: (event) {
-                  // send Offset(x, y) to server
-                  final data = {
-                    "type": "moveMouse",
-                    "dx": event.delta.dx,
-                    "dy": event.delta.dy
-                  };
-                  this.channel.sink.add(json.encode(data));
-                },
+                onPointerMove: (event) => this
+                    .hanldePointerMove(event.position.dx, event.position.dy),
                 onPointerDown: (event) {
-                  this.channel.sink.add(json.encode({
+                  this._prevX = event.position.dx;
+                  this._prevY = event.position.dy;
+
+                  this.widget.channel.sink.add(json.encode({
                         "type": "mouseClick",
                         "button": "left",
                       }));
@@ -55,15 +71,15 @@ class Mouse extends StatelessWidget {
             children: [
               MouseButton(
                 text: "LMB",
-                channel: this.channel,
+                channel: this.widget.channel,
               ),
               MouseButton(
                 text: "MMB",
-                channel: this.channel,
+                channel: this.widget.channel,
               ),
               MouseButton(
                 text: "RMB",
-                channel: this.channel,
+                channel: this.widget.channel,
               ),
             ],
           )
