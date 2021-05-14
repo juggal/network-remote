e4a93b28ff3adcc2503e9fd0f16da5e592101288
nihar <dasnihar48@gmail.com>
2021-05-15 01:17:17 +0530

    separate websocket  server in python

---
+++ services/mouse_controller.py
@@ -0,0 +1,25 @@
+import asyncio
+import websockets
+import json
+import pyautogui as pag
+
+pag.FAILSAFE = False
+pag.PAUSE = 0
+
+
+def mouse_move(message):
+    data = json.loads(message)
+    dx = data["dx"]
+    dy = data["dy"]
+    print(dx, dy)
+    pag.moveRel(dx, dy)
+
+
+async def server(websocket, path):
+    async for message in websocket:
+        mouse_move(message)
+
+start_server = websockets.serve(server, "0.0.0.0", 8001)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
