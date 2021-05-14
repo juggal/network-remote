955f157c2fa436a9967d972ec7aec2bd785542eb
nihar <dasnihar48@gmail.com>
2021-05-14 22:15:36 +0530

    mousepad testing

--- ui/lib/views/mouse.dart
+++ ui/lib/views/mouse.dart
@@ -1,9 +1,8 @@
-import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:network_remote/utils/base_url.dart';
 import 'package:network_remote/widgets/mouse_button.dart';
-import 'package:http/http.dart' as http;
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class Mouse extends StatefulWidget {
   @override
@@ -11,26 +10,22 @@ class Mouse extends StatefulWidget {
 }
 
 class _MouseState extends State<Mouse> {
-  double _prevX;
-  double _prevY;
+  // double _prevX;
+  // double _prevY;
 
-  var client = http.Client();
+  // void hanldePointerMove(X, Y, client) async {
+  //   double deltaX = X - this._prevX;
+  //   double deltaY = Y - this._prevY;
 
-  void hanldePointerMove(X, Y, client) async {
-    double deltaX = X - this._prevX;
-    double deltaY = Y - this._prevY;
+  //   this._prevX = X;
+  //   this._prevY = Y;
 
-    this._prevX = X;
-    this._prevY = Y;
-
-    final data = {"dx": deltaX, "dy": deltaY};
-    try {
-      await client.post(BaseUrl.getUri("/mouse/move"), body: json.encode(data));
-    } catch (e) {
-      // Navigator.pushReplacementNamed(context, "/address");
-      print(e);
-    }
-  }
+  //   final data = {"dx": deltaX, "dy": deltaY};
+  //   try {
+  //     await client.post(BaseUrl.getUri("/mouse/move"), body: json.encode(data));
+  //   } catch (e) {
+  //     print(e);
+  //   }
 
   @override
   Widget build(BuildContext context) {
@@ -43,16 +38,13 @@ class _MouseState extends State<Mouse> {
             child: ConstrainedBox(
               constraints: BoxConstraints.tight(const Size.fromWidth(450)),
               child: Listener(
-                onPointerMove: (event) => this.hanldePointerMove(
-                    event.position.dx, event.position.dy, client),
+                // onPointerMove: (event) => this.hanldePointerMove(
+                //     event.position.dx, event.position.dy, client),
                 onPointerDown: (event) {
-                  this._prevX = event.position.dx;
-                  this._prevY = event.position.dy;
-
-                  // this.widget.channel.sink.add(json.encode({
-                  //       "type": "mouseClick",
-                  //       "button": "left",
-                  //     }));
+                  // this._prevY = event.position.dy;
+                  // this._prevX = event.position.dx;
+                  print("hello");
+                  IO.io("http://192.168.1.7:8001/socket.io")                  
                 },
                 child: Container(
                   decoration: BoxDecoration(
